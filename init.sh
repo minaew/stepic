648071a455401a7a0657ca5092858e8d9aecb348
Alexey Minaev <22275840+minaew@users.noreply.github.com>
2021-04-24 18:33:13 +0300

    Added step for gunicorn

--- init.sh
+++ init.sh
@@ -1,12 +1,13 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-# sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-# sudo /etc/init.d/gunicorn restart
+
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
 
-mkdir -p /home/box/web/public/img
-mkdir -p /home/box/web/public/css
-mkdir -p /home/box/web/public/js
-mkdir -p /home/box/web/uploads
-mkdir -p /home/box/web/etc
+# mkdir -p /home/box/web/public/img
+# mkdir -p /home/box/web/public/css
+# mkdir -p /home/box/web/public/js
+# mkdir -p /home/box/web/uploads
+# mkdir -p /home/box/web/etc
